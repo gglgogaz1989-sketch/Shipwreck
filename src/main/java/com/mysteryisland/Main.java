b94a031e3b863150ebc0099bcba550022b18d60f
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-21 23:14:03 +0200

    Update Main.java

--- src/main/java/com/mysteryisland/Main.java
+++ src/main/java/com/mysteryisland/Main.java
@@ -1,12 +1,7 @@
 package com.mysteryisland;
 
-import com.mysteryisland.ui.UI;
-
 public class Main {
     public static void main(String[] args) {
-        UI.clearScreen();
-        UI.printTitle();
-        
         Game game = new Game();
         game.start();
     }
