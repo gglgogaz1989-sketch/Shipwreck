6fe75cb5a5259e08dc9fcf4e1cada51c35d41442
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-21 23:17:38 +0200

    Create Colors.java

---
+++ src/main/java/com/mysteryisland/ui/Colors.java
@@ -0,0 +1,10 @@
+package com.mysteryisland.ui;
+
+public class Colors {
+    public static final String RESET = "\033[0m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String RED = "\033[31m";
+    public static final String CYAN = "\033[36m";
+    public static final String WHITE = "\033[37m";
+}
