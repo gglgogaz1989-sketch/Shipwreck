5b250ac33fa29ccb0e1e1d9ff692c041b8fcea7c
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-21 23:27:16 +0200

    Update Input.java

--- src/main/java/com/mysteryisland/utils/Input.java
+++ src/main/java/com/mysteryisland/utils/Input.java
@@ -14,6 +14,28 @@ public class Input {
         }
     }
     
+    public static int getInt(String prompt) {
+        System.out.print(prompt);
+        while (!scanner.hasNextInt()) {
+            System.out.print("❌ Введите число: ");
+            scanner.next();
+        }
+        int value = scanner.nextInt();
+        scanner.nextLine();
+        return value;
+    }
+    
+    public static int getInt(String prompt, int min, int max) {
+        int value;
+        do {
+            value = getInt(prompt);
+            if (value < min || value > max) {
+                System.out.println("❌ Введите от " + min + " до " + max);
+            }
+        } while (value < min || value > max);
+        return value;
+    }
+    
     public static String getString(String prompt) {
         System.out.print(prompt);
         return scanner.nextLine();
